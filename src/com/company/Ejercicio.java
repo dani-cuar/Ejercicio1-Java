122b5168ca7434e1b898f0311c6299c9428a7ebd
Daniela C <daniela.cuartasm@udea.edu.co>
2022-11-03 18:01:54 -0500

    Ejercicio java tipos de datos

---
+++ src/com/company/Ejercicio.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Ejercicio {
+
+    public static void main(String[] args) {
+
+        //Tipos de datos e impresion
+
+        // 1.0 Numericos
+        int numero1 = 5;
+        System.out.println("Esto es un int: "+ numero1);
+
+        long numero2 = 1;
+        System.out.println("Esto es un long: " + numero2);
+
+        float decimal = 3.1f;
+        System.out.println("Esto es un float: "+ decimal);
+
+        double num = 4.5d;
+        System.out.println("Esto es un double: " + num);
+
+        //2.0 Texto
+        String nombre = "Daniela";
+        System.out.println("Esto es un nombre: " + nombre);
+
+        char letra = 'j';
+        System.out.println("Esto es un char: " + letra);
+
+        //3.0 Booleano
+        boolean verd = true;
+        System.out.println("Esto es un booleano: " + verd);
+
+        boolean falso = false;
+        System.out.println("Esto es un booleano: " + falso);
+    }
+}
